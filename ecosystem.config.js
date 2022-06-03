623ad96ac4d74d48715ecea0de3a630f7c509fe2
gyurilee <jae04099@gmail.com>
2022-06-03 18:16:33 +0900

    fix : edit ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'nodejs-sequelize-pm2',
             script: './src/app.js',
-            instances: 1, 
+            instances: 0, 
             exec_mode: 'cluster',
             merge_logs: true, 
             autorestart: true, 
