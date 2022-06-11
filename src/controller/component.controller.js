35fa09d3df19da32b06ced065c57f8dad2ec3c03
HyunHo Lee <dlgusgh2001@naver.com>
2022-06-11 19:54:59 +0900

    feat: template id - relation component

--- src/controller/component.controller.js
+++ src/controller/component.controller.js
@@ -3,12 +3,16 @@ const Component = db.Component;
 const Op = db.sequelize.Op;
 
 exports.update = (req, res) => {
-    const id = req.params.id;
-    Component.increment({ like: 1 }, { where: { id } })
-        .then(() => {
-            res.send({ res: true })
-        })
-        .catch((err) => {
-            console.error(err);
-        })
-}
+  const id = req.params.id;
+  Component.increment({ like: 1 }, { where: { id } })
+    .then(() => {
+      res.send({ res: true });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+exports.relation = (req, res) => {
+  const id = req.params.id;
+};
--- src/routes/template.js
+++ src/routes/template.js
@@ -1,23 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const models = require('../models/index');
-const templateController = require('../controller/template.controller');
+const models = require("../models/index");
+const templateController = require("../controller/template.controller");
 
-router.get('/', (req, res, next) => {
-    models.Template.findAll({
-        // where: {
-        //     deletedAt: null
-        // }
-        include: [{
-            model: models.TemplateTags
-        }]
-    })
-        .then((template) => {
-            res.status(200).json(template);
-        });
+router.get("/", (req, res, next) => {
+  models.Template.findAll({
+    // where: {
+    //     deletedAt: null
+    // }
+    include: [
+      {
+        model: models.TemplateTags,
+      },
+    ],
+  }).then((template) => {
+    res.status(200).json(template);
+  });
 });
 
-router.put('/update/:id', templateController.update);
+router.get("/:id", (req, res, next) => {
+  models.Component.findAll({
+    // where: {
+    //     deletedAt: null
+    // }
+    include: [
+      {
+        model: models.ComponentTags,
+      },
+    ],
+    where: { related_template: req.params.id },
+  }).then((component) => {
+    res.status(200).json(component);
+  });
+});
 
+router.put("/update/:id", templateController.update);
 
 module.exports = router;
